3208964501c0a14826d1b16a1d733484859adfc2
ramya <ramya@RAMYA.ht.home>
2023-06-23 13:46:56 -0400

    Commited one more sysout in AnjanClass2

--- src/com/anjan/AnjanClass2.java
+++ src/com/anjan/AnjanClass2.java
@@ -5,6 +5,7 @@ public class AnjanClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi Ramya , I cloned your Project");
+System.out.println("Added one more sysout");
 	}
 
 }
